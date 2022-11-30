45b7613bd603b2af6ba5974bb9c855173a586485
zajkiss <89912921+zajkiss@users.noreply.github.com>
2022-11-30 13:11:37 +0200

    atnaujinimas

---
+++ 2022-11-30/script.js
@@ -0,0 +1,25 @@
+const x = 'Jonas';
+const y = 'Jonaitis';
+
+console.log( x[0]+'.'+y);
+
+
+    const vardas = 'Lukas';
+const pavarde = 'Zajačauskas';
+const GimimoMetai = 1994;
+const EinamiejiMetai = 2022;
+const Amzius = EinamiejiMetai - GimimoMetai;
+console.log(`Aš esu ${vardas} ${pavarde}. Man yra ${Amzius} metai.`);
+
+const vardelis = 'Lukas';
+const pavardelis = "Zajančauskas";
+const Tryspaskutiniai = '';
+const vardelisIlgis = vardelis.length-1;
+const pavardelisIlgis = pavardelis.length-1;
+
+console.log('Trys paskutinės vardo raidės '+vardelis[vardelisIlgis-2],vardelis[vardelisIlgis-1], vardelis[vardelisIlgis]);
+console.log('Trys paskutinės pavardės raidės '+pavardelis[pavardelisIlgis-2],pavardelis[pavardelisIlgis-1], pavardelis[pavardelisIlgis]);
+
+
+
+

c5d3adadd5f2fcbc6e8817bba56feb1a616551d1
zajkiss <89912921+zajkiss@users.noreply.github.com>
2023-03-28 19:14:15 +0300

    update

---
+++ internetine-parduotuve/backend/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+
+class ProductsController extends Controller
+{
+   public function index(){
+    $data = Products::all();
+
+    return $data;
+   }
+
+   public function create(Request $request) {
+      try {
+
+         $product = new Products;
+
+         $product->name = $request->name;
+         $product->sku = $request->sku;
+         $product->photo = $request->photo;
+         $product->warehouse_qty = $request->warehouse_qty;
+         $product->price = $request->price;
+
+         $product->save();
+         
+         return 'Produktas sėkmingai  sukurtas';
+      }catch(\Exception $e) {
+         return response('Nepavyko išsaugoti produkto', 500);
+      }
+
+   }
+   public function edit( $id, Request $request) {
+      try {
+
+
+         $product = Products::find($id);
+
+         $product->name = $request->name;
+         $product->sku = $request->sku;
+         $product->photo = $request->photo;
+         $product->warehouse_qty = $request->warehouse_qty;
+         $product->price = $request->price;
+
+         $product->save();
+         
+         return 'Sekmingai atnaujinta';
+      }catch(\Exception $e) {
+         return response($e);
+      }
+
+   }
+
+
+
+
+   public function delete($id) {
+      try {
+     Products::find($id)->delete();
+      return 'Produktas sėkmingai ištrintas';
+      } catch(\Exception $e) {
+         return response('Atsiprašome įvyko klaida', 500);
+
+      }
+
+
+   }
+   public function result($id) {
+      try {
+         $data =  Products::find($id);
+      return $data;
+      } catch(\Exception $e) {
+         return response('Atsiprašome įvyko klaida', 500);
+
+      }
+
+
+   }
+
+}
---
+++ internetine-parduotuve/backend/routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//CRUD
+//CREATE - POST
+//READ   - GET
+//UPDATE - PUT
+//DELETE - DELETE
+
+Route::group(['prefix'=>'products'],function(){
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::post('/', [ProductsController::class, 'create']);
+    Route::put('/{id}', [ProductsController::class, 'edit']);
+    Route::get('/{id}', [ProductsController::class, 'result']);
+
+
+
+    Route::delete('/{id}', [ProductsController::class, 'delete'])->where('id', '[0-9]+');
+});
